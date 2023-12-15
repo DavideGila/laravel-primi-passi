214c64f118b72da15e665213aba7e2f4fe235cf5
DavideGila <davide.gila@gmail.com>
2023-12-15 14:26:12 +0100

    added $data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'greeting' => 'Hello',
+        'name' => 'World'
+    ];
+    return view('home', $data);
 });
